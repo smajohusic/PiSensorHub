c80f14a81c1715ac2426787f18d2ecedf22d2785
Smajo Husic <smajohusic@gmail.com>
2018-07-15 11:59:41 +0200

    no message

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import sys
 import time
-#import Adafruit_DHT
+import Adafruit_DHT
 
 app = Flask(__name__)
 app.debug = True
@@ -12,7 +12,18 @@ def main():
 
 @app.route('/api/temperature-and-humidity')
 def temperatureAdnHumidity():
-    return 'New values';
+    sensor = Adafruit_DHT.DHT22
+    pin = 23
+
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    if humidity is not None and temperature is not None:
+        return jsonify({
+                'temperature' => '{0:0.1f}'.format(temperature),
+                'humidity' => '{1:0.1f}'.format(humidity),
+            })
+
+    return null
 
 if __name__ == "__main__":
     app.run()
