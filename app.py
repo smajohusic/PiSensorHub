b47c947026bdbb800b28ed0f679e1419e9e57fe1
Smajo Husic <smajohusic@gmail.com>
2018-08-17 17:47:22 +0200

    no message

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def main():
 
 @app.route('/api/temperature-and-humidity')
 def temperatureAdnHumidity():
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(2302, 4)
 
     if humidity is not None and temperature is not None:
         return json.dumps({'temperature': temperature, 'humidity': humidity});
